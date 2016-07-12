efd654aecf6c86f30c65fb68a78ddd9f7e849a85
krajnm01 <michal.krajnak0@gmail.com>
2016-07-12 13:50:53 +0200

    My first Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+aqua_note
+=========
+
+A Symfony project created on July 12, 2016, 8:31 am.
---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: krajnm01
+ * Date: 12/07/2016
+ * Time: 13:38
+ */
+
+namespace AppBundle\Controller;
+
+
+class GenusController
+{
+
+}
